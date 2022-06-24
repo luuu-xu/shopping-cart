3ebb4744e1502a16a0f65749c3c11708a0d4e224
luuu-xu <a550763321@gmail.com>
2022-06-24 11:58:27 -0400

    Added basename to BroswerRouter.

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import CartPage from './components/CartPage';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/'>
       <Routes>
         <Route path='/' element={<App />}>
           <Route index element={<HomePage />}/>
